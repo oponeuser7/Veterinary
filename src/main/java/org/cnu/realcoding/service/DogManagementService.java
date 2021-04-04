5bdbdc7ab612602463212c88b3beee657831e2f6
kanghyejong <qpsxpsltm@naver.com>
2021-04-04 23:34:57 +0900

    real repair post and getAllKey

--- src/main/java/org/cnu/realcoding/service/DogManagementService.java
+++ src/main/java/org/cnu/realcoding/service/DogManagementService.java
@@ -1,45 +1,38 @@
 package org.cnu.realcoding.service;
 
 import org.cnu.realcoding.domain.Dog;
-import lombok.Getter;
-import org.cnu.realcoding.controller.DogController;
-import org.cnu.realcoding.domain.Dog;
+import org.cnu.realcoding.exception.DogAlreadyExistsException;
 import org.cnu.realcoding.exception.DogNotFoundException;
 import org.cnu.realcoding.repository.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class DogManagementService {
 
     @Autowired
-    private DogRepository dogrepo;
-
-    public Dog getDogByPhoneNum(String PhoneNum){
-        return dogrepo.getDogByPhoneNum(PhoneNum);
-    }
-
     private DogRepository dogRepository;
 
-    public List<Dog> getNameDogs(String name) {
-        dogs.contains(name);
-        return dogs;
-    }
-
     public void insertDog(Dog dog) {
-        dogRepository.postDogs(dog);
+        if(dogRepository.check_Exist(dog)){
+            System.out.println("이미 존재하는 강아지입니다.");
+            throw new DogAlreadyExistsException();
+        }else{
+            dogRepository.postDogs(dog);
+        }
     }
 
-    public Dog getDogByName(String name) {
-        for (Dog dog : dogs) {
-            if (dog.getName().equals(name)) {
-                return dog;
-            }
+    public Dog getDogByAllKey(Dog dog){
+        Dog mongo_dog = dogRepository.getDogByAllKey(dog);
+        if(mongo_dog != null) {
+            return mongo_dog;
+        }else {
+            System.out.println("존재하지 않는 강아지입니다. ");
+            throw new DogNotFoundException();
         }
-
-        throw new DogNotFoundException();
+    }
+    public Dog getDogByPhoneNum(String PhoneNum){
+        return dogRepository.getDogByPhoneNum(PhoneNum);
     }
 }
