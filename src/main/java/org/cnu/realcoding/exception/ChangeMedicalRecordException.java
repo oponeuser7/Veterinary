ce524a5cfecfe082ce0d3ebe06f0d223e6ab1aca
saltwalks <ewqdsacxz2345@gmail.com>
2021-04-04 21:05:20 +0900

    add exceptions & repository

---
+++ src/main/java/org/cnu/realcoding/exception/ChangeMedicalRecordException.java
@@ -0,0 +1,8 @@
+package org.cnu.realcoding.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+public class ChangeMedicalRecordException extends RuntimeException{
+}
