5d936805fef25d6dfd876701a39e285006f42097
kanghyejong <qpsxpsltm@naver.com>
2021-04-04 22:31:31 +0900

    post and key_check

--- src/main/java/org/cnu/realcoding/service/DogManagementService.java
+++ src/main/java/org/cnu/realcoding/service/DogManagementService.java
@@ -1,4 +1,43 @@
 package org.cnu.realcoding.service;
 
+import lombok.Getter;
+import org.cnu.realcoding.controller.DogController;
+import org.cnu.realcoding.domain.Dog;
+import org.cnu.realcoding.exception.DogNotFoundException;
+import org.cnu.realcoding.repository.DogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class DogManagementService {
+
+    @Autowired
+    private DogRepository dogRepository;
+
+    public List<Dog> getNameDogs(String name) {
+        dogs.contains(name);
+        return dogs;
+    }
+
+    public void insertDog(Dog dog) {
+        dogRepository.postDogs(dog);
+    }
+
+    public Dog getDogByName(String name) {
+        for (Dog dog : dogs) {
+            if (dog.getName().equals(name)) {
+                return dog;
+            }
+        }
+
+        throw new DogNotFoundException();
+    }
+
+
+
+
+
 }
