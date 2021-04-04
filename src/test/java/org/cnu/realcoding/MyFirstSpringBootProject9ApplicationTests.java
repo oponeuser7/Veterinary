7cf42545f4a0b1e2ec2cbd14797233a2fe356995
lavgin7 <oponeuser7@o.cnu.ac.kr>
2021-04-04 17:23:11 +0900

    first commit

---
+++ src/test/java/org/cnu/realcoding/MyFirstSpringBootProject9ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringBootProject9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
